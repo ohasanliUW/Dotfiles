d259da78a23ea216a8ba19a42000015f6e6ebf1d
Orkhan Hasanli <ohasanli@yahoo.ca>
2024-06-20 15:36:00 -0400

    smart split

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,45 +1,65 @@
-local wezterm = require 'wezterm';
+local w = require('wezterm')
+
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+local function is_vim(pane)
+  -- this is set by the plugin, and unset on ExitPre in Neovim
+  return pane:get_user_vars().IS_NVIM == 'true'
+end
+
+-- if you *ARE* lazy-loading smart-splits.nvim (not recommended)
+-- you have to use this instead, but note that this will not work
+-- in all cases (e.g. over an SSH connection). Also note that
+-- `pane:get_foreground_process_name()` can have high and highly variable
+-- latency, so the other implementation of `is_vim()` will be more
+-- performant as well.
+local function is_vim(pane)
+  -- This gsub is equivalent to POSIX basename(3)
+  -- Given "/foo/bar" returns "bar"
+  -- Given "c:\\foo\\bar" returns "bar"
+  local process_name = string.gsub(pane:get_foreground_process_name(), '(.*[/\\])(.*)', '%2')
+  return process_name == 'nvim' or process_name == 'vim'
+end
+
+local direction_keys = {
+  h = 'Left',
+  j = 'Down',
+  k = 'Up',
+  l = 'Right',
+}
+
+local function split_nav(resize_or_move, key)
+  return {
+    key = key,
+    mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+    action = w.action_callback(function(win, pane)
+      if is_vim(pane) then
+        -- pass the keys through to vim/nvim
+        win:perform_action({
+          SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
+        }, pane)
+      else
+        if resize_or_move == 'resize' then
+          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+        end
+      end
+    end),
+  }
+end
+
 return {
-  color_scheme = "Dracula",
-  -- colors = {
-  --   background = "#0c0e14",
-  -- },
-  window_decorations = "NONE",
-  font = wezterm.font("Iosevka"),
-  font_size = 10.0,
-  -- dpi = 192.0,
-  font_antialias = "Subpixel", -- None, Greyscale, Subpixel
-  font_hinting = "Full",  -- None, Vertical, VerticalSubpixel, Full
-  leader = { key="a", mods="CTRL" },
-  hide_tab_bar_if_only_one_tab = true,
   keys = {
-    { key = "a", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
-    { key = "-", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-    { key = "\\",mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-    { key = "s", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-    { key = "v", mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-    { key = "o", mods = "LEADER",       action="TogglePaneZoomState" },
-    { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
-    { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
-    { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
-    { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
-    { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
-    { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
-    { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
-    { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
-    { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
-    { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
-    { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
-    { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
-    { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
-    { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
-    { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
-    { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
-    { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
-    { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
-    { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
-    { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
-    { key = "d", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
-    { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
+    -- move between split panes
+    split_nav('move', 'h'),
+    split_nav('move', 'j'),
+    split_nav('move', 'k'),
+    split_nav('move', 'l'),
+    -- resize panes
+    split_nav('resize', 'h'),
+    split_nav('resize', 'j'),
+    split_nav('resize', 'k'),
+    split_nav('resize', 'l'),
   },
 }
+
