c16acceed70957b89089b9954bc7cf25d61cc3fb
Orkhan Hasanli <ohasanli@yahoo.ca>
2024-06-22 13:05:37 -0400

    rename

---
+++ config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,3 @@
+--git clone https://github.com/LazyVim/starter ~/.config/nvim Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
