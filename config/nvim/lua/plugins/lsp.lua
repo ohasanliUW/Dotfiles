ae396467b9a2cbfee2a076e33e342de3358b0f48
Orkhan Hasanli <ohasanli@yahoo.ca>
2024-07-31 07:56:28 -0400

    udpate lsp config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -28,109 +28,110 @@ return {
       pyright = {
         enabled = false,
       },
+      ruff = {
+        enabled = true,
+      },
       ruff_lsp = {
         enabled = false,
       },
       pylance = {},
+      gopls = {},
       rust_analyzer = {},
-      --     pylsp = {
-      --       plugins = {
-      --         pyflakes = {
-      --           enabled = false,
-      --         },
-      --         mccabe = {
-      --           enabled = false,
-      --         },
-      --         rope = {
-      --           enabled = true,
-      --         },
-      --         ruff = {
-      --           enabled = true,
-      --           executable = "ruff",
-      --           formatEnabled = true,
-      --           format = { "I" },
-      --           unsafeFixes = false,
-      --           lineLength = 120,
-      --           targetVersion = "py311",
-      --         },
-      --         pycodestyle = {
-      --           enabled = false,
-      --         },
-      --       },
-      --     },
+    },
+    setup = {
+      pylance = function(_, opts)
+        local cmp_lsp = require("cmp_nvim_lsp")
+        opts.capabilities =
+          vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
+      end,
+      ruff = function(_, opts)
+        local cmp_lsp = require("cmp_nvim_lsp")
+        opts.capabilities =
+          vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
+        opts.cmd = { "/home/ohasanli/.pyenv/shims/ruff", "server", "--preview" } -- Add the --preview option
+        opts.on_attach = function(client, bufnr)
+          if client.name == "ruff" then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ async = true })
+              end,
+            })
+          end
+        end
+      end,
+      lua_ls = function(_, opts)
+        local cmp_lsp = require("cmp_nvim_lsp")
+        opts.capabilities =
+          vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
+        opts.settings = {
+          Lua = {
+            runtime = { version = "Lua 5.1" },
+            diagnostics = {
+              globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+            },
+          },
+        }
+      end,
     },
   },
-  -- config = function()
-  --   local cmp_lsp = require("cmp_nvim_lsp")
-  --   local capabilities =
-  --     vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
-  --
-  --   require("mason").setup()
-  --   require("mason-lspconfig").setup({
-  --     ensure_installed = {
-  --       "lua_ls",
-  --       "rust_analyzer",
-  --       "gopls",
-  --       "zk",
-  --       -- "ruff",
-  --       -- "pylsp",
-  --       -- "pylyzer",
-  --     },
-  --     handlers = {
-  --       function(server_name) -- default handler (optional)
-  --         require("lspconfig")[server_name].setup({
-  --           capabilities = capabilities,
-  --         })
-  --       end,
-  --       -- ["pylsp"] = function()
-  --       --   local lspconfig = require("lspconfig")
-  --       --   lspconfig.pylsp.setup({
-  --       --     capabilities = capabilities,
-  --       --     settings = {
-  --       --       pylsp = {
-  --       --         plugins = {
-  --       --           mccabe = {
-  --       --             enabled = false,
-  --       --           },
-  --       --           pyflakes = {
-  --       --             enabled = false,
-  --       --           },
-  --       --           pycodestyle = {
-  --       --             enabled = false,
-  --       --           },
-  --       --           rope = {
-  --       --             enabled = true,
-  --       --           },
-  --       --           rope_autoimport = {
-  --       --             enabled = true,
-  --       --           },
-  --       --           -- ruff = {
-  --       --           --   enabled = true,
-  --       --           --   executable = "ruff",
-  --       --           --   formatEnabled = true,
-  --       --           --   format = { "I" },
-  --       --           --   unsafeFixes = false,
-  --       --           --   lineLength = 120,
-  --       --           --   targetVersion = "py311",
-  --       --           -- },
-  --       --         },
-  --       --       },
-  --       --     },
-  --       --   })
-  --       -- end,
-  --       -- ["pylyzer"] = function()
-  --       --   local lspconfig = require("lspconfig")
-  --       --
-  --       --   lspconfig.pylyzer.setup({
-  --       --     name = "pylyzer",
-  --       --     cmd = { "pylyzer", "--server" },
-  --       --     filetypes = { "python" },
-  --       --     root_dir = function()
-  --       --       return vim.env.ATT
-  --       --     end,
-  --       --   })
-  --       -- end,
-  --     },
-  --   })
-  -- end,
+  config = function(_, opts)
+    local cmp = require("cmp")
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+    require("fidget").setup({})
+    -- require("mason").setup()
+    -- require("mason-lspconfig").setup({
+    --   ensure_installed = {
+    --     "lua_ls",
+    --     "rust_analyzer",
+    --     "gopls",
+    --     "ruff",
+    --   },
+    -- })
+
+    local lspconfig = require("lspconfig")
+    for server, server_opts in pairs(opts.servers) do
+      if server_opts.enabled ~= false then
+        local final_opts = vim.tbl_deep_extend("force", {}, server_opts)
+        if opts.setup[server] then
+          opts.setup[server](nil, final_opts)
+        end
+        lspconfig[server].setup(final_opts)
+      end
+    end
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" }, -- For luasnip users.
+      }, {
+        { name = "buffer" },
+      }),
+    })
+
+    vim.diagnostic.config({
+      -- update_in_insert = true,
+      float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
+  end,
 }
